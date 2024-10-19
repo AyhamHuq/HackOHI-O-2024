7cb15bd562bd38751ff296fc85278b2ead919a21
Jacob McLachlan <jacobmclachlan@Jacobs-MacBook-Pro-2.local>
2024-10-19 16:53:37 -0400

    run n descriptions against a list of lists of your choosing

--- analysis/embeddings.py
+++ analysis/embeddings.py
@@ -6,6 +6,20 @@ import time
 from fetcher import fetcher
 import sqlite3
 
+#region settings
+
+# which columns are we running the analysis on?
+# names must match a .txt file in ../data/keywords
+attrs = ['low', 'medium', 'high']
+
+# are we running on the entire dataset?
+entire_dataset = False
+
+# if we're not running everything, how many descriptions are we running
+n_descriptions = 30
+
+#endregion
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "../processing"))
 from sql import sql
@@ -17,10 +31,11 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 current_dir = os.getcwd()
 assert current_dir == script_dir, (f"script must be run from {script_dir}")
 
+total_t = time.time()
+
 con = sqlite3.connect(sql.db_name)
 cursor = con.cursor()
 
-attrs = ['low', 'medium', 'high']
 for attr in attrs: sql.add_score(cursor, attr)
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
@@ -29,10 +44,10 @@ model = AutoModel.from_pretrained('jinaai/jina-embeddings-v3', trust_remote_code
 descriptions = fetcher.get_descriptions(cursor)
 
 # for file in os.listdir(data_folder):
-for file in attrs:
+for attr in attrs:
     t = time.time()
 
-    file_path = os.path.join(data_folder, f'{file}.txt')
+    file_path = os.path.join(data_folder, f'{attr}.txt')
     
     with open(file_path, 'r') as input:
         phrases = input.read().lower().split("\n")
@@ -41,23 +56,17 @@ for file in attrs:
         index = faiss.IndexFlatL2(dimension)
         index.add(embeddings)
 
-        i = 0
-        # for desc, embedding in zip(descriptions, descriptions_embeddings):
-        for desc in descriptions:
-            if i > 20:
-                break
-            i += 1
+        for (i, desc) in enumerate(descriptions):
+            if i >= n_descriptions and not entire_dataset: break
 
             description_embedding = model.encode(desc.text)
             D, I = index.search(description_embedding.reshape(1, -1), k=1)
 
-            print(D[0][0])
-            cursor.execute(sql.update_score(file), (D[0][0], desc.pk1, desc.pk2))
+            cursor.execute(sql.update_score(attr), (float(D[0][0]), desc.pk1, desc.pk2))
 
-
-
-
-    print(f'took {time.time() - t} seconds\n')
+    print(f'{attr} took {time.time() - t} seconds')
 
 con.commit()
 con.close()
+
+print(f'\ntotal time: {time.time() - total_t} seconds\n')
