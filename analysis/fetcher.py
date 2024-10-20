4941d71108386aca8042332eb8c1194c06f03a7b
Ayham Huq <huq.19@buckeyemail.osu.edu>
2024-10-19 21:24:56 -0400

    Added lemmatization.

--- analysis/fetcher.py
+++ analysis/fetcher.py
@@ -4,6 +4,9 @@ import sys
 from collections import namedtuple
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, '../processing'))
@@ -15,14 +18,17 @@ data = namedtuple('data', ['pk1', 'pk2', 'text'])
 class fetcher:
 
     def filter_stop_words(text: str) -> str:
-        # # Initialize stop words
-        # stop_words = set(stopwords.words('english'))
-        # # Tokenize the text (split into words)
-        # tokens = text.split()
-        # # Remove stop words
-        # filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
-        # return ' '.join(filtered_tokens)
-        return text
+        lemmatizer = WordNetLemmatizer()
+
+        # Initialize stop words
+        stop_words = set(stopwords.words('english'))
+        # Tokenize the text (split into words)
+        tokens = text.split()
+        # Remove stop words
+        filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+        filtered_tokens = [lemmatizer.lemmatize(word) for word in tokens]
+
+        return ' '.join(filtered_tokens)
 
 
     def get_descriptions(cursor:sqlite3.Cursor) -> list[data]:
@@ -45,6 +51,7 @@ class fetcher:
                 risk_notes, 
                 risk_follow_up or ''
             ]))
+
             l.append(data(observation_no, point_name, descriptiion))
 
         return l
